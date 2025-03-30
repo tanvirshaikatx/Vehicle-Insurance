2aab522baeb8de98591d6e5a63891b7db7207e40
tanvirshaikatx <tanvirshaikatx.com>
2025-03-30 20:38:28 +0600

    template

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Tanvir Shaikat",
+    author_email="tanvirshaikatx@gmail.com",
+    packages=find_packages()
+)
